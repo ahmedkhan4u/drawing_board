766f394f7492241f60275119ee37c6a5cef518d8
Ahmad-PC <ahmedkhan871871@gmail.com>
2022-09-08 14:40:24 +0500

    Drawing Board Scribble Package

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Drawing Board',
       theme: ThemeData(
         primarySwatch: Colors.blue,
